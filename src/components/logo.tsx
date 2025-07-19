80956ef6bb3b3ff61dc310f6adc3ee8f765880d8
Camly <camlysales@gmail.com>
2025-07-19 19:15:34 +0000

    logo is not showing

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,12 +3,12 @@ import Image from 'next/image';
 
 export function LogoIcon({ className }: { className?: string }) {
   return (
-    <div className={cn("relative size-18", className)}>
+    <div className={cn("relative size-9", className)}>
       <Image
         src="/logo.png"
         alt="Camly Logo"
         fill
-        sizes="72px"
+        sizes="36px"
         className="object-contain"
       />
     </div>
