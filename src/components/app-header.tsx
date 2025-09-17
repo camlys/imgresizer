667b5d489f2bdd238c47a9acd33dcda7316c374a
Camly <camlysales@gmail.com>
2025-09-17 15:45:27 +0000

    keep image logo less transparent

--- src/components/app-header.tsx
+++ src/components/app-header.tsx
@@ -122,7 +122,7 @@ export function AppHeader({
   return (
     <header className="flex items-center justify-between p-4 pl-6 border-b bg-card overflow-hidden">
       <Link href="/" className="flex items-center gap-2 relative">
-        <LogoIcon className="absolute top-1/2 -translate-y-1/2 text-primary/10 size-12 left-0 -translate-x-1/4 md:size-16 md:left-1/2 md:-translate-x-1/2" />
+        <LogoIcon className="absolute top-1/2 -translate-y-1/2 opacity-20 size-12 left-0 -translate-x-1/4 md:size-16 md:left-1/2 md:-translate-x-1/2" />
         <div className="sun-rays pl-4">
             <h1 className="text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-primary via-accent to-primary bg-[size:200%_auto] animate-gradient-shift font-headline tracking-tight">
                 ImgResizer
