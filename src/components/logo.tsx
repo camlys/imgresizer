ec5d65caf7ac5b6c3da8816313d8330d813fc863
Camly <camlysales@gmail.com>
2025-09-24 22:03:22 +0000

    sin header image logo camly.png is not showing that is in public folder

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -4,8 +4,8 @@ import { cn } from "@/lib/utils";
 export function LogoIcon({ className }: { className?: string }) {
   return (
     <img
-      src="/ImgResizer.png"
-      alt="ImgResizer Logo"
+      src="/camly.png"
+      alt="Camly Logo"
       className={cn(className)}
     />
   );
