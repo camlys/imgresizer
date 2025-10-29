287e174a339ef6c0c7a40d59342c73ba9c514db4
Camly <camlysales@gmail.com>
2025-10-29 17:36:15 +0000

    remove text logo from header and show image logo size professional

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -10,11 +10,8 @@ export function SiteHeader() {
   return (
     <header className="py-3 px-6 bg-card border-b sticky top-0 z-10 overflow-hidden">
       <div className="container mx-auto flex items-center justify-between">
-        <Link href="https://www.imgresizer.xyz/" className="flex flex-col md:flex-row md:items-center md:gap-3">
-          <LogoIcon className="size-8 md:size-9" />
-          <div className="text-sm -mt-1 md:mt-0 md:text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-primary via-accent to-primary bg-[size:200%_auto] animate-gradient-shift font-headline tracking-tight">
-              ImgResizer
-          </div>
+        <Link href="https://www.imgresizer.xyz/" className="flex items-center gap-3">
+          <LogoIcon className="size-10" />
         </Link>
         <div className="flex items-center gap-2">
           <Popover>
